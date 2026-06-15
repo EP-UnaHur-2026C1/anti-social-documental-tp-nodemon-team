952712b6683276703ca507f4054d70733b3483a1
Toms200319 <mezatomas63@gmail.com>
2026-06-15 15:54:01 -0300

    Definición de campos comment y tag para los posts

--- src/models/postSchema.js
+++ src/models/postSchema.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: "User",required: true},
-    comment:{},
-    tag:{},
+    comment:{type: mongoose.Schema.Types.ObjectId, ref: "Comment", required: true},
+    tag:{type: mongoose.Schema.Types.ObjectId, ref: "Tag", required: true},
     postImages: [
         {
             url: {type: String, required: true}
