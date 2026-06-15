f0d425860b62c9dbbf59c398f5374439cd1a7a14
Toms200319 <mezatomas63@gmail.com>
2026-06-15 16:35:33 -0300

    Cambios en el archivo main.js para poder conectarse a la base de datos

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("UnaHur - Anti-Social net");
+const express = require('express')
+const connectToDatabase = require('./config/db')
+const postRoutes = require('./routes/postRoutes')
+
+const app = express()
+
+app.use(express.json());
+app.use('/post', postRoutes);
+
+async function startServer() {
+  await connectToDatabase(); 
+  app.listen(3000, () => {
+    console.log("App iniciada");
+  });
+}
+
+startServer();
