b2166d38434f6faf3a913e77a0fb5ed41eaf3bce
Toms200319 <mezatomas63@gmail.com>
2026-06-16 19:51:05 -0300

    Creación de rutas para el modelo User

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const {Router} = require('express')
+const router = Router()
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../controllers/userController')
+
+router.get('/', getUsers)
+router.get('/:id', getUserById)
+router.post('/', createUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
+
+module.exports = router
