b57c7ecde6f52fa379f7a10b9058bfbf33aa106b
tobiasramirez <tobi05cabj@gmail.com>
2026-06-19 10:03:11 -0300

    Añado las rutas de comment al main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const connectToDatabase = require('./config/db')
 const postRoutes = require('./routes/postRoutes')
+const commentRoutes = require("./routes/commentRoutes")
 
 const app = express()
 
 app.use(express.json());
 app.use('/post', postRoutes);
+app.use("/comment",commentRoutes)
 
 async function startServer() {
   await connectToDatabase(); 
