83897dd870d32e5edfa108ee45caa3eba4b7a2e2
tobiasramirez <tobi05cabj@gmail.com>
2026-06-19 11:59:26 -0300

    Ya no es required tag en postSchema

--- src/models/postSchema.js
+++ src/models/postSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: "User",required: true},
-    tags:{type: mongoose.Schema.Types.ObjectId, ref: "Tag", required: true}
+    tags:{type: mongoose.Schema.Types.ObjectId, ref: "Tag"}
 },
  {
     timestamps: true
