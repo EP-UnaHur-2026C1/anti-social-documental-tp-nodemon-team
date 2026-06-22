ab3e77d610d0fddc6de1e39291d72b6dc0304328
tobiasramirez <tobi05cabj@gmail.com>
2026-06-22 11:16:14 -0300

    Arreglo al postcontroller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,25 @@
 const Post = require('../models/postSchema');
 const PostImage = require("../models/postImageSchema")
+const Comment = require("../models/commentSchema")
+
+const calcularLimiteVisibilidad = () => {
+  const limiteMeses = Number(process.env.COMMENT_VISIBILITY_MONTHS) || 6;
+  const fechaLimite = new Date();
+  fechaLimite.setMonth(fechaLimite.getMonth() - limiteMeses);
+  return fechaLimite;
+}
+const resolverTagIds = async (tags = []) => {
+  return Promise.all(
+    tags.map(async (tagName) => {
+      const tag = await Tag.findOneAndUpdate(
+        { name: tagName },
+        { $setOnInsert: { name: tagName } },
+        { upsert: true, new: true }
+      );
+      return tag._id;
+    })
+  );
+}
 
 const getPost = async (req, res) => {
     try{
@@ -16,21 +36,24 @@ const getPostById = async (req, res) => {
         const id = req.params.id
         const post = await Post.findById(id).populate("user", "nickname").populate("tags", "nombre")
         const images = await PostImage.find({ post: post._id })
-        const commments = await Comment.find({
+        const comments = await Comment.find({
             post: post._id,
+            createdAt: { $gte: calcularLimiteVisibilidad() }
             }).populate("user", "nickName")
-        res.status(200).json(post)
+        res.status(200).json({...post.toJSON(),images,comments})
     }
     catch(error){
-        res.status(500),json(error.message)
+        res.status(500).json(error.message)
     }
 }
 
 const createPost = async (req, res) => {
     try{
-        const post = req.body
-        const newPost = await Post.create(post)
-        res.status(201).json(newPost)
+        const {user,contenido,tags=[]} = req.body
+        const tagIds = await resolverTagIds(tags)
+        const newPost = await Post.create({user,contenido,tags: tagIds})
+        const post = await Post.findById(newPost._id).populate("user", "nickname").populate("tags", "nombre");
+        res.status(201).json(post)
     }
     catch(error){
         res.status(500).json(error.message)
@@ -51,7 +74,10 @@ const updatePost = async (req, res) =>{
 const deletePost = async (req, res) =>{
     try{
         const id = req.params.id
-        const post = await Post.findByIdAndDelete(id)
+        const post = await Post.findById(id)
+        await Comment.deleteMany({ post: post._id });
+        await PostImage.deleteMany({ post: post._id });
+        await Post.findByIdAndDelete(post._id);
         res.status(200).json({message: "Post eliminado"})
     }
     catch(error){
