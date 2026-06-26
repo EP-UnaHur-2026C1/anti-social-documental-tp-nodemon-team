37a5d113f9b53e51271cbff49ced821761264586
Agustin Santiago Frecha <asfrecha05@gmail.com>
2026-06-26 00:42:07 -0300

    Revisar bonus

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
-
 const userSchema = new mongoose.Schema({
-    nickname: {type:String, required: true, unique: true}
+    nickname: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
 })
-
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
